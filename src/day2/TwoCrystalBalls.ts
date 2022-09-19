3f6c7313a52a3f0972fbb69f8231f768d16b236e
Greg Schoenberg <gregschoenberg@gmail.com>
2022-09-18 21:07:11 -0700

    solved BinarySearchLists

--- src/day2/TwoCrystalBalls.ts
+++ src/day2/TwoCrystalBalls.ts
@@ -1,3 +1,10 @@
 export default function two_crystal_balls(breaks: boolean[]): number {
 
 }
+
+// pass in sorted array of booleans, false first, return number, which index is
+// the last index a ball can be dropped from without breaking.
+// have two crystal balls
+// first go to sqrt of N, if ball doesn't break, increase sqrt of N until end
+// if ball breaks, linear search from previous sqrt.
+// time complexity O(sqrt(N))
